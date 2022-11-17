f634313a52a2eac8fa8f97c739ce72ed012459b7
anup-kgp <dasanup1999.com>
2022-11-18 00:48:28 +0530

    added

---
+++ main.js
@@ -0,0 +1,6 @@
+import { navbar } from "./components/navbar.js";
+let nav=document.getElementById("navv");
+nav.innerHTML=navbar()
+import { footer } from "./components/navbar.js";
+let foot=document.getElementById("foot");
+foot.innerHTML=footer();
